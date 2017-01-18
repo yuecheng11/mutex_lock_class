f6ee4afb6319ee46f10774623930d073817e5bd3
yuecheng11 <yuecheng011@sina.cn>
2017-01-18 14:23:40 +0800

    upload

---
+++ Cmutexlock.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+#include <unistd.h>
+#include<pthread.h>
+#include <errno.h>
+using namespace std;
+
+class MutexLock
+{
+public:
+	MutexLock();
+	~MutexLock();
+	void lock();
+	void unlock();
+private:
+	void mutexinit();
+	void mutexDestroy();
+private:
+	pthread_mutex_t m_Mutex;
+};
+
+MutexLock::MutexLock()
+{
+	mutexinit();
+}
+
+MutexLock::~MutexLock()
+{
+	mutexDestroy();
+
+}
+
+void MutexLock::mutexinit()
+{
+	pthread_mutex_init(&m_Mutex,NULL);
+}
+
+void MutexLock::mutexDestroy()
+{
+	pthread_mutex_destroy(&m_Mutex); 
+}
+
+void MutexLock::lock()
+{
+	pthread_mutex_lock(&m_Mutex);
+}
+
+void MutexLock::unlock()
+{
+	pthread_mutex_unlock(&m_Mutex);
+}
+
+MutexLock g_Mutex;
+
+void *testMsg(void* testMsg)
+{
+	char* msg = (char*)testMsg;
+
+	g_Mutex.lock();
+
+	for(int i = 0;i < 5;++i)
+	{
+		cout<<msg<<" : "<<i+1<<endl;
+		sleep(1);
+	}
+	g_Mutex.unlock();
+
+	return NULL;
+}
+int main()
+{
+	pthread_t ptest1,ptest2;
+
+	pthread_create(&ptest1,NULL,&testMsg,(void*)"this first thread");
+	pthread_create(&ptest2,NULL,&testMsg,(void*)"this second thread");
+
+	pthread_join(ptest1, NULL);                                 
+    pthread_join(ptest2, NULL); 
+
+	return 0;
+}
